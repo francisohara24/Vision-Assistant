4b20f273f12d701c582f19f2a40249a38f737468
francisohara24 <franciskohara@gmail.com>
2023-11-26 11:34:54 -0500

    Style: organize code and add comment

--- scripts/rpi_ocr/rpi_ocr.py
+++ scripts/rpi_ocr/rpi_ocr.py
@@ -1,5 +1,6 @@
 """Test Tesseract-OCR on Raspberry Pi."""
 import pytesseract
 
-
-print(pytesseract.image_to_string("data/image_2.png"))
+# extrect text from image_2
+text = pytesseract.image_to_string("data/image_1.png")
+print(text)
