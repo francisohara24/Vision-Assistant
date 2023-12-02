3a8f8773e6ff31ffde562d7fcd25afdb65e0edbd
francisohara24 <franciskohara@gmail.com>
2023-12-02 13:16:05 -0500

    Feat: try to change voice for reading text.

--- scripts/rpi_tts/rpi_tts_pyttsx3.py
+++ scripts/rpi_tts/rpi_tts_pyttsx3.py
@@ -6,9 +6,17 @@ engine = pyttsx3.init()
 # set the speech rate to a slower rate than the default
 engine.setProperty("rate", 170)
 
+# get all available voices for TTS
+voices = engine.getProperty("voices")
+
+# set voice for TTS
+engine.setProperty("voices", voices[0].id)
+
 # set input text to be converted
 input_text = "Hello World! This is a text to speech model running on the Raspberry Pi 4!"
 
 # convert input text to speech format and play aloud
 engine.say(input_text)
 engine.runAndWait()
+
+for voice in voices: print(voice.id)
