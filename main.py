bafca1c870cb8b20803a43e2c8cfdb8db61c3123
francisohara24 <franciskohara@gmail.com>
2023-12-03 04:00:37 -0500

    Feat: update main script for controlling the device.

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-import ai_reader
+from ai_reader import ocr
+from ai_reader import capture_image
+from ai_reader import proximity
+from ai_reader import tts
+from gpiozero import Button
+import asyncio
+import time
+
+
+# define buttons for triggering pipelines
+ocr_button = Button(17)
+ultrasonic_button = Button(27)
+
+
+# coroutine function for indefinite OCR sequence
+async def ocr_pipeline():
+    """Run the text recognition and speech synthesis pipeline of the project."""
+    while True:
+        if ocr_button.is_pressed:
+            image = capture_image.capture()
+            text = ocr.extract_text(image)
+            tts.say(text)
+            await asyncio.sleep(0.25)
+
+
+# coroutine function for indefinite Ultrasonic distance sequence
+async def ultrasonic_pipeline():
+    """Run the ultrasonic proximity detection pipeline of the project."""
+    while True:
+        if ultrasonic_button.is_pressed:
+            distance = proximity.check_proximity()
+            tts.say(f"You are {distance} centimeters away from the nearest object.")
+
+
+# schedule pipeline coroutines as tasks in top-level coroutine
+async def main():
+    """Top-level coroutine for all pipelines"""
+    await asyncio.gather(ocr_pipeline(), ultrasonic_pipeline())
+
+# run top level coroutine
+asyncio.run(main())
