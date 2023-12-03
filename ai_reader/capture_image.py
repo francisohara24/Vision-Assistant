c0a9b2b594fdd414c432a3e163b1b3f54d394d52
francisohara24 <franciskohara@gmail.com>
2023-12-03 02:42:31 -0500

    Feat: add main script for capturing images w/ raspberry pi camera

---
+++ ai_reader/capture_image.py
@@ -0,0 +1,17 @@
+"""Script for capturing image using raspberry Pi camera"""
+from picamera2 import Picamera2
+from libcamera import controls
+import time
+
+# instantiate camera object
+camera = Picamera2()
+
+# turn on camera
+camera.start()
+
+# enable camera autofocus for clearer images
+camera.set_controls({"AfMode": 2, "AfTrigger": 0})
+
+def capture():
+    # capture an image with the camera and return it as a Pillow Image
+    return camera.capture_image()
