1692509bf4b580da5bc37b9aa9089a38ba66f83e
francisohara24 <franciskohara@gmail.com>
2023-12-03 00:35:43 -0500

    Feat: add script for reading input text using current text to speech model.

---
+++ ai_reader/tts.py
@@ -0,0 +1,29 @@
+"""Function for converting extracted text into synthesized speech."""
+import pyttsx3
+
+
+def say(text_input: str) -> None:
+    """Read text_input aloud to the user using the device's current text-to-speech model.
+
+    Parameters
+    ----------
+    text_input : str
+        The text to be read aloud to the user.
+
+    Returns
+    -------
+    None
+    """
+
+    # instantiate text to speech engine
+    tts = pyttsx3.init()
+
+    # set appropriate speech rate for the model
+    tts.setProperty("rate", 180)
+
+    # read the input text aloud
+    tts.say(text_input)
+    tts.runAndWait()
+
+if __name__ == "__main__":
+    say("Hello! I am AI-Reader, and I will be your reading assistant for today.")
