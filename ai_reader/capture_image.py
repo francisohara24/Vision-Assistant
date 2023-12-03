0cef2d3c215f651c95d038e2570cb477e3f2008d
francisohara24 <franciskohara@gmail.com>
2023-12-03 05:08:40 -0500

    Fix: add code for debugging ocr pipeline on raspberry pi.

--- ai_reader/capture_image.py
+++ ai_reader/capture_image.py
@@ -15,5 +15,5 @@ camera.set_controls({"AfMode": 2, "AfTrigger": 0})
 
 def capture() -> Image:
     """Capture an image with the camera and return it as a Pillow Image."""
-    camera.capture_file("image.jpg")  # test code
+    camera.capture_file("image2.jpg")  # test code
     return camera.capture_image()
