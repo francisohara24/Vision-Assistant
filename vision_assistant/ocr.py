fa140efe7f883c394f8cc58da21be81f5ef5bf82
francisohara24 <franciskohara@gmail.com>
2023-12-04 16:06:12 -0500

    Docs: update comments
    update comments and remove use of Pillow image library in implementation of `extract_offline()`.

--- vision_assistant/ocr.py
+++ vision_assistant/ocr.py
@@ -1,28 +1,41 @@
 """Function for recognizing and extracting text from a Pillow Image using the current OCR model.
-Current model: Tesseract-OCR (https://github.com/tesseract-ocr/tesseract)
+Offline model: Tesseract-OCR (https://github.com/tesseract-ocr/tesseract)
+Online model: Google Vision API (https://cloud.google.com/vision?hl=en)
 """
+# import required libraries
 import pytesseract
-from PIL import Image
 import requests
 from google.cloud import vision
 
 
-def extract_text(image):
-    """Check if device is online or offline and call the appropriate extract_text function"""
-    request = requests.get("http://clients3.google.com/generate_204")
-    if request.status_code == 204:
-        return extract_online(image)
-    else:
-        return extract_offline(image)
-
-
-def extract_offline(image_path: str) -> str:
-    """Extract and return any text contained within `image` using the offline text recognition model.
+def extract_text(image_path: str) -> str:
+    """Check if device is online or offline and call the appropriate extract_text function
 
     Parameters
     ----------
-    image : PIL.Image.Image
-        The image containing text to be extracted using the OCR model.
+    image_path : str
+        The path to an image file containing text.
+
+    Returns
+    -------
+    str
+        The text extracted from the image using either the offline or online ocr function.
+    """
+    # check if connected to internet
+    request = requests.get("http://clients3.google.com/generate_204")
+    if request.status_code == 204:
+        return extract_online(image_path)  # when online
+    else:
+        return extract_offline(image_path)  # when offline
+
+
+def extract_offline(image_path: str) -> str:
+    """Extract and return any text contained within image located at `image_path` using the offline text recognition model.
+
+    Parameters
+    ----------
+    image_path : str
+        The path to the image containing text to be extracted using the OCR model.
 
     Returns
     -------
@@ -30,39 +43,61 @@ def extract_offline(image_path: str) -> str:
         The extracted text if any text was recognized.
         If not, the string "No text was detected" is returned instead.
     """
-    image = Image.open(image_path)
-    text = pytesseract.image_to_string(image)
+    # extract text from image using Tesseract-OCR
+    text = pytesseract.image_to_string(image_path)
 
+    # return extracted text or prompt user if no text could be found
     if len(text) > 0:
         return text
-
     return "No text was detected!"
 
 
 def extract_online(image_path: str) -> str:
-    """Extract and return any text contained within image located at `image_path` using the cloud-based text recognition model."""
+    """Extract and return any text contained within image located at `image_path` using the cloud-based text recognition
+     model.
+
+     Parameters
+    ----------
+    image_path : str
+        The path to the image containing text to be extracted using the OCR model.
+
+    Returns
+    -------
+    str
+        The extracted text if any text was recognized.
+        If not, the string "No text was detected" is returned instead.
+        If there was an error with the API request, the string "Sorry. Please try again." is returned instead.
+
+    """
 
     # instantiate google vision client for text recognition
     client = vision.ImageAnnotatorClient()
 
-    # open image file to be read as binary and extract content
+    # open image file to be read and extract file content
     with open(image_path, "rb") as image_file:
         content = image_file.read()
 
     # convert image to google vision image format
     image = vision.Image(content=content)
 
-    # send a request for text recognition on the image to the google vision API
+    # send request for text recognition using the client
     response = client.text_detection(image=image)
 
-    # extract actual text from API response
+    # check if request was unsuccessful.
+    if response.error.message:
+        return "Sorry. Please try again."
+
+    # if request successful, extract actual text from API response
     text = response.text_annotations[0].description
 
-    # join the extracted texts into a single text and return
-    return text
+    # return extracted text or prompt user if no text could be found.
+    if len(text) > 0:
+        return text
+    return "No text was detected."
 
 
+# main script for testing the module
 if __name__ == "__main__":
-    image = Image.open("../data/rpi_images/image_26.jpg")
-    result = extract_text(image)
+    image_path = "../data/rpi_images/image_26.jpg"
+    result = extract_text(image_path)
     print(result)
