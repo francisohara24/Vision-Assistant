931a9dda6b73aa1303992a6527c28e9aa9e34637
francisohara24 <franciskohara@gmail.com>
2023-12-04 11:39:42 -0500

    Chore: change directory for main package of the project.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
+# import required libraries
 from ai_reader import ocr
 from ai_reader import capture_image
 from ai_reader import proximity
 from ai_reader import tts
 from gpiozero import Button
-from PIL import Image
 import threading
 import time
 
 
-# define buttons for triggering pipelines
+# define GPIO buttons for triggering each pipeline
 ocr_button = Button(17)
 ultrasonic_button = Button(27)
 
@@ -18,9 +18,16 @@ def ocr_pipeline():
     """Run the text recognition and speech synthesis pipeline of the project."""
     while True:
         if ocr_button.is_pressed:
+            # capture an image with the camera
             image_path = capture_image.capture()
+
+            # extract text from the captured image
             text = ocr.extract_text(image_path)
+
+            # print the extracted text for remote debugging purposes with SSH
             print(text)
+
+            # read aloud the extracted text
             tts.say(text)
             time.sleep(1)
 
@@ -37,6 +44,7 @@ def ultrasonic_pipeline():
 
 # schedule pipeline coroutines as tasks in top-level coroutine
 if __name__ == "__main__":
+    tts.say("Power On!")
     ocr_thread = threading.Thread(target=ocr_pipeline)
     ultrasonic_thread = threading.Thread(target=ultrasonic_pipeline)
     ocr_thread.start()
